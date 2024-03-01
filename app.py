de12e2a655a75d27add76909f693ea3666a21fc9
Gabi <gabriele.tonello@mail.polimi.it>
2024-03-01 12:45:22 +0100

    init commit

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return """
+        <html>
+            <head>
+                <title>
+                    Test flask
+                </title>
+            </head>
+            <body>
+                <h1>ciao mondo</h1>
+            </body>
+        </html>
+        """
+
+if __name__ == "__main__": 
+    app.run(debug=True, port= 8080)
